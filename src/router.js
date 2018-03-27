ccb2ab4d0c3f147db77997ae17b013a540082a19
i2xzy <yitzchokr@live.com>
2018-03-27 17:49:05 +0100

    add handler for userPhoto and api request function relates #4 #7

--- src/router.js
+++ src/router.js
@@ -1,19 +1,17 @@
-const staticHandler = require('./handler');
+const { staticHandler, photoHandler } = require('./handler');
 
-const router = (req, response) => {
+const router = (req, res) => {
   const { url } = req;
 
   if (url === '/') {
-    staticHandler(response, 'public/index.html');
-  } else if (url === '/dailyphoto') {
-    photoHandler(response, url);
-  } else if (url === '/userphoto') {
-    photoHandler(response, url);
-  } else if (url.indexOf('public') !== -1) {
-    staticHandler(response, url);
+    staticHandler('public/index.html', res);
+  } else if (url.startsWith('/api/search')) {
+    photoHandler(req, res);
+  } else if (url.startsWith('/public')) {
+    staticHandler(url, res);
   } else {
-    response.writeHead(404, { 'content-type': 'text/plain' });
-    response.end('404 error');
+    res.writeHead(404, { 'content-type': 'text/plain' });
+    res.end('404 error');
   }
 };
 
--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
 const http = require('http');
 const router = require('./router');
 
-require('dotenv').config();
-
-const key = process.env.APIKEY;
 const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 3000;
 
