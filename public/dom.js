08a5c99a7cdab96fbb50fba9dec93842e725b013
Jennah <contactjennah@gmail.com>
2018-03-28 10:16:39 +0100

    Add event listener and callback func for date field relates #5

---
+++ public/dom.js
@@ -0,0 +1,5 @@
+/* eslint-disable */
+function createImg(url) {
+  var img = document.querySelector('img');
+  img.setAttribute('src', url);
+}
--- public/index.js
+++ public/index.js
@@ -1,15 +1,23 @@
+/* eslint-disable */
+
 // client side generic XML request func to make requests to the server 
 function makeRequest(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-            var response = xhr.responseText;
-            callback(response);
-        } else {
-            console.log(`Status code ${xhr.status}`)
-        }
-    })
-    xhr.open('GET', url)
-    xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener('load', function () {
+    if (xhr.status === 200) {
+      var response = xhr.responseText;
+      callback(response);
+    } else {
+      console.log(`Status code ${xhr.status}`);
+    }
+  })
+  xhr.open('GET', url);
+  xhr.send();
 }
 
+// Event listener for date input, makes request to server on change 
+var dateField = document.querySelector('#date');
+dateField.addEventListener('input', function(){
+  console.log('date field stuff: ', dateField.value)
+  makeRequest('/api/search?' + dateField.value, createImg)
+})
